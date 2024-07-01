a98e72995b7efb2bb12c4668c0ac9ed97f8553cb
Denny <zhongsen.wu@ibm.com>
2024-07-01 14:11:37 +0800

    feat: create a react native project with expo

---
+++ Expo/StickerSmash/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: '#fff' }}>Open up App.js to start working on your app!</Text>
+      <Text>Hello React Native and Expo~</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#25292e',
+    color: '#f5f5dc',
+  },
+});
